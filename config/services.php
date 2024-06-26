a607c1fa675298acbf91f57aa317379baffb39e9
daimakabe <makabe@a-cial.com>
2024-06-26 10:17:45 +0900

    一覧画面へいいね数とコメント数の表示

--- config/services.php
+++ config/services.php
@@ -30,5 +30,10 @@ return [
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
+    
+    'line' => [
+        'channel_token' => env('LINE_CHANNEL_TOKEN'),
+        'channel_secret' => env('LINE_CHANNEL_SECRET'),
+    ],
 
 ];
